060fcd6e9d52dfd8dce1a936a1cf557dac852ef9
Somaku <somaku@makuragusa-ya.com>
2025-11-07 09:33:18 +0800

    feat(webui): use BackgroundTasks

--- ani_sort/web/routes/tasks.py
+++ ani_sort/web/routes/tasks.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Request, Form, HTTPException
+import threading
+from fastapi import APIRouter, Request, Form, HTTPException, BackgroundTasks
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from ani_sort.db import SessionLocal, Task, WatchedFolder
 from ani_sort.task import run_sort_task
 
-router = APIRouter( tags=["Tasks"])
+
+router = APIRouter(tags=["Tasks"])
 templates = Jinja2Templates(directory="ani_sort/web/templates")
 
 
@@ -27,14 +29,19 @@ def list_tasks(request: Request):
     )
 
 
+def long_sort_task(folder_path: str):
+    print(f"Sorting started for {folder_path}")
+    run_sort_task(input_path=folder_path)
+    print(f"Sorting completed for {folder_path}")
+
+
 @router.post("/run")
-def run_task_from_web(request: Request, folder_id: int = Form(...)):
+def run_task_from_web(background_tasks: BackgroundTasks, folder_id: int = Form(...)):
     db = SessionLocal()
     folder = db.query(WatchedFolder).filter_by(id=folder_id).first()
     if not folder:
         raise HTTPException(status_code=404, detail="Folder not found")
 
-    run_sort_task(
-        input_path=folder.path,
-    )
+    background_tasks.add_task(long_sort_task, folder.path)
+
     return RedirectResponse("/", status_code=303)
